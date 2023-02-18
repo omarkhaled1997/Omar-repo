5bba915fd461cbc741ccfa91d457651f6bb549e9
omarkhaled1997 <debaticandproud@gmail.com>
2023-02-18 11:53:13 +0200

    version1

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  int x,y,a,b,c,d,e;
+  printf("please enter the values \n");
+  scanf("%d%d",&x,&y);
+   a=x+y;
+   b= x-y;
+   c= x*y;
+   d= x/y;
+   e= x%y;
+ printf("The sum of the two values = %d\n",a);
+  printf("The difference of the two values = %d\n",b);
+   printf("The multiplication of the two values = %d\n",c);
+    printf("The division of the two values = %d\n",d);
+     printf("The reminder of the two values = %d\n",e);
+
+
+
+}
